d70e485463f31336b4ad8247791b92731e44cf8e
Jansi Chothani <jansichothani288@gmail.com>
2026-02-06 11:33:59 +0530

    Resolved merge conflict in App.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
@@ -33,39 +32,3 @@ const Header = () => {
 };
 
 export default Header;
-=======
-import { Link } from "react-router-dom";
-import "../styles/Header.css";
-
-const Header = () => {
-  return (
-    <header className="header">
-      <div className="container header-inner">
-        <div className="logo-img">
-          <h2>
-            FashionBloom
-            </h2>
-          {/* <img src="/Logo (3).png" alt="" /> */}
-           <span>curated fashion</span>
-        </div>
-
-        <nav className="nav">
-          <Link to="/">Home</Link>
-          <Link to="/category/men">Men</Link>
-          <Link to="/category/women" >Women</Link>
-          <Link to="/category/kids">Kids</Link>
-          <Link to="/category/accessories">Accessories</Link>
-        </nav>
-
-        <div className="header-icons">
-          <span>♡</span>
-          <span>👤</span>
-          <span>🛒</span>
-        </div>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
->>>>>>> 2591678f67b9df7a12de2aeaef5945965f7b1f81
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "../styles/Sidebar.css";
 
 const Sidebar = () => {
@@ -27,33 +26,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-=======
-import "../styles/Sidebar.css";
-
-const Sidebar = () => {
-  return (
-    <aside className="sidebar">
-      <h4>Search</h4>
-      <input type="text" placeholder="Search items" />
-
-      <h4>Sort</h4>
-      <select>
-        <option>Featured</option>
-        <option>Price: Low to High</option>
-        <option>Price: High to Low</option>
-      </select>
-
-      <h4>Price</h4>
-      <input type="range" min="0" max="1200" />
-      <div className="price-range">
-        <span>AED 0</span>
-        <span>AED 1200</span>
-      </div>
-
-      <button className="clear-btn">Reset Filters</button>
-    </aside>
-  );
-};
-
-export default Sidebar;
->>>>>>> 2591678f67b9df7a12de2aeaef5945965f7b1f81
